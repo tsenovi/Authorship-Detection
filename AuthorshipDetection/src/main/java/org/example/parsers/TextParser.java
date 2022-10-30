1864ddae431e8f2db3ae26b363b194e5e920d183
tsenovi <vandertsen@gmail.com>
2022-10-30 15:12:06 +0200

    Implemented method for counting sentences in text

--- AuthorshipDetection/src/main/java/org/example/parsers/TextParser.java
+++ AuthorshipDetection/src/main/java/org/example/parsers/TextParser.java
@@ -16,29 +16,61 @@ public class TextParser {
   }
 
   public ParsedText parse(String paragraph) {
+    if (paragraph.isEmpty()) {
+      return null;
+    }
+
+    String cleanText = cleanUpText(paragraph);
     ParsedText parsedText = new ParsedText();
 
-    parsedText.setWordCount(calculateWordCount(paragraph));
+    parsedText.setWordCount(calculateWordCount(cleanText));
+    parsedText.setSentenceCount(calculateSentenceCount(cleanText));
 
     return parsedText;
   }
 
   private int calculateWordCount(String text) {
     String textToParse = new String(text);
-    if (textToParse.isEmpty()) {
-      return 0;
-    }
-
-    final String[] words = cleanPunctuation(textToParse);
+    final String[] words = cleanAllPunctuation(textToParse);
     return words.length;
   }
 
-  private String[] cleanPunctuation(String text) {
-    return text.toLowerCase()
-        .replaceAll("\\p{P}", "")
-        .replaceAll("\n", "")
-        .replaceAll("\r\n", "")
-        .replaceAll("\r", "")
+  private String[] cleanAllPunctuation(String text) {
+    return text.replaceAll("\\p{P}", "")
         .split("\\s+");
+  }
+
+  private int calculateSentenceCount(String text) {
+    String textToParse = new String(text);
+    int abbreviationCount = calculateAbbreviationCount(textToParse);
+    final String[] sentences = cleanPunctuationForSentence(textToParse);
+    int sentenceCount = sentences.length - 1;
+    return sentenceCount - abbreviationCount;
+  }
+
+  private String[] cleanPunctuationForSentence(String text) {
+    return text.split("[.!?]+");
+  }
+
+  private int calculateAbbreviationCount(String text) {
+    String[] abbreviations = {"mr.", "mrs.", "miss.", "dr."};
+    String[] words = text.split("\\s+");
+    int count = 0;
+    for (String word : words) {
+      for (String abbreviation : abbreviations) {
+        if (word.contains(abbreviation)) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }
+
+  private String cleanUpText(String text) {
+    return text.toLowerCase()
+        .replaceAll("\r", "")
+        .replaceAll("\n", "\\s+")
+        .replaceAll("\r\n", "\\s+");
   }
 }
