88f022800cdc431cbdf494394dfff4206132cf24
tsenovi <vandertsen@gmail.com>
2022-11-02 02:07:45 +0200

    Added weights to the FeatureType enums

--- AuthorshipDetection/src/main/java/org/example/expressions/FeatureType.java
+++ AuthorshipDetection/src/main/java/org/example/expressions/FeatureType.java
@@ -1,9 +1,19 @@
 package org.example.expressions;
 
 public enum FeatureType {
-  AVERAGE_WORD_LENGTH,
-  TYPE_TOKEN_RATIO,
-  HAPAX_LEGOMENA_RATIO,
-  AVERAGE_SENTENCE_LENGTH,
-  AVERAGE_SENTENCE_COMPLEXITY
+  AVERAGE_WORD_LENGTH(11),
+  TYPE_TOKEN_RATIO(33),
+  HAPAX_LEGOMENA_RATIO(50),
+  AVERAGE_SENTENCE_LENGTH(0.4),
+  AVERAGE_SENTENCE_COMPLEXITY(4);
+
+  private final double weight;
+
+  FeatureType(double weight) {
+    this.weight = weight;
+  }
+
+  public double getWeight() {
+    return weight;
+  }
 }
